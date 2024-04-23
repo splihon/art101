fcd163a3a46071e234ba0977fbb4c187717977c6
Segolen Plihon <131832637+splihon@users.noreply.github.com>
2024-04-23 11:23:24 -0700

    lab 5

--- lab5/js/lab.js
+++ lab5/js/lab.js
@@ -1,6 +1,22 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - Data Types & Variable
+// Author: Segolen Plihon
+// Date:4/23/2024
+
+// Define Variable
+make = "volkswagen";
+model = "beetle";
+color = "brown";
+year = 2017;
+
+// calculate
+age = 2024 - year;
+
+// output
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + " years<br>");
 
 // Constants
 
