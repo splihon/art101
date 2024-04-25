151a13f5210e76036d377269ea2ce7dadbd9077f
Segolen Plihon <131832637+splihon@users.noreply.github.com>
2024-04-25 15:12:07 -0700

    lab 6

--- lab6/js/lab.js
+++ lab6/js/lab.js
@@ -1,8 +1,28 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - Arrays and Objects
+// Author: Segolen Plihon
+// Date: 4/25/24
 
-// Constants
+//Define Variables
+myTransport = ["Volkswagen", "Bus", "Scooter", "Rides from Friends"];
+
+//create an object for my main ride
+myMainRide = {
+  make: "Volkswagen",
+  model: "Bettle",
+  color: "Brown",
+  year: "2017",
+  //define a function within our object (a method)
+  //that uses the value of year above (using this.year)
+  age: function() {
+    return 2022 - this.year;
+  }
+}
+
+//output
+document.writeln("Kinds of transportation I use: ", myTransport, "</br>");
+// this little trick allows us to write an object to the document
+document.writeln("My Main Ride: <pre>",
+JSON.stringify(myMainRide, null, '\t'), "</pre>");
 
 // Functions
 
