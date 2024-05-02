521fc826ca98a6ec12b0f70eb8f69e4e82e8c1bd
Segolen Plihon <131832637+splihon@users.noreply.github.com>
2024-05-01 19:01:59 -0700

    debugging 2

--- lab7/js/lab.js
+++ lab7/js/lab.js
@@ -5,27 +5,22 @@
 // Constants
 
 // Functions
-//Function to sort user's name
-function sortUserName(){
-    var userName = window.prompt("Hi. Please tell me your name so I can fix it.");
+
+// Functions
+
+// Function to sort user's name
+function sortUserName(userName) {
     console.log("userName =", userName);
-    //Split string to array
     var nameArray = userName.split('');
     console.log("nameArray =", nameArray);
-    //Sort the array
     var nameArraySort = nameArray.sort();
     console.log("nameArraySort =", nameArraySort);
-    //Join array back to a string
     var nameSorted = nameArraySort.join('');
     console.log("nameSorted =", nameSorted);
-    //Note the above lines could have been written as one line"
-    // userName.toLower().split("").sort().join("")
     return nameSorted;
 }
 
-//Bonus code for bonus task
-//Ignore/Omit Spaces
-//Function to sort user's name ignoring spaces
+// Function to sort user's name ignoring spaces
 function sortNameIgnoringSpaces(userName) {
     var cleanedName = userName.replace(/\s/g, '').split('').sort().join('');
     return cleanedName;
@@ -44,20 +39,3 @@ document.writeln("Sorted name without spaces:", cleanedAndSortedName, "<br>");
 
 // Optionally, you can also console log the cleaned and sorted name
 console.log("Sorted name without spaces:", cleanedAndSortedName);
-
-
-// function sortNameIgnoringSpaces(userName) {
-//     var cleanedName = userName.replace(/\s/g, '').split('').sort().join('');
-//     return cleanedName;
-// }
-
-// //Ask for user's name once
-// var userName = window.prompt("Enter your name:");
-
-// //Using user's name in both functions
-// var sortedName = sortUserName(userName);
-// var cleanedAndSortedName = sortNameIgnoringSpaces(userName);
-
-// // Output
-// document.writeln("Oh hey, I've fixed your name: ", sortUserName(), "</br>");
-
